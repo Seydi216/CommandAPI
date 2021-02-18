9d55b46e2ebfbdbeacbc4a2cefe7628777faada9
Seydi <seydi.sapayev.sss@gmail.com>
2021-02-18 09:39:24 +0300

    Initial Commit

---
+++ preparing/Controllers/CommandController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace preparing.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommandController: ControllerBase
+    {
+       [HttpGet]
+       public ActionResult<IEnumerable<string>> Get() {
+            return new string[] { "this", "is", "hard", "coded" };
+        }
+    }
+}
