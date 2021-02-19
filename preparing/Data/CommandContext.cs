2b212867f9d45219479a4e2ab149facfed7b21ea
Seydi <seydi.sapayev.sss@gmail.com>
2021-02-19 18:14:05 +0300

    Added CommandContext

---
+++ preparing/Data/CommandContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using preparing.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace preparing.Data
+{
+    public class CommandContext: DbContext
+    {
+        public CommandContext(DbContextOptions<CommandContext> options)
+            : base(options) {
+
+        }
+        public DbSet<Command> CommandItems { get; set; }
+    }
+}
